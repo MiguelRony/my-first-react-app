a107aeb6255af5aba9b42bd948da04bcf175cc12
MiguelRony <miguel.rony@icloud.com>
2025-02-07 16:35:36 -0600

    practice

---
+++ src/Greeting.jsx
@@ -0,0 +1,6 @@
+
+function Greeting() {
+    return <h1 style={{backgroundColor: 'white', color:'black'}}>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>;
+  }
+
+export default Greeting;
